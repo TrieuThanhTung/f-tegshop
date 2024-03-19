e949592fef918b6b74b0d41b849a31069bf62f5b
TrieuThanhTung <tung.ttrieu@gmail.com>
2024-03-19 17:36:35 +0700

    add infor-user

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,6 +2,8 @@
 import styles from './Profile.module.scss';
 import React from 'react';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +16,51 @@ const Profile = () => {
       </header>
       <div className={cx("container")}>
         <div className={cx("profile")}>
-          <table>
+          <table className={cx("profile-table")}>
+            <tr>
+              <td>First Name</td>
+              <td>
+                <input type="text" value={'first name'}/>
+              </td>
+            </tr>
+            <tr>
+            <td>Last Name</td>
+              <td>
+                <input type="text" value={'Last name'}/>
+              </td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td className={cx("change-email")}>
+                <p style={{paddingRight: '0.5rem'}}>
+                  tung.ttrieu@gmail.com
+                </p>
+                <Link to={"/change-email"} style={{textDecoration: 'underline'}}>Change</Link>
+              </td>
+            </tr>
+            <tr>
+              <td>Password</td>
+              <td className={cx("change-password")}>
+                <p style={{paddingRight: '0.5rem'}}>
+                  *********
+                </p>
+                <Link to={"/change-email"} style={{textDecoration: 'underline'}}>Change</Link>
+              </td>
+            </tr>
+            <tr>
+              <td>Birthday</td>
+              <td>
+                <input type="date" />
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>
+                <Button type='primary' size='large' loading={false}>
+                  Save
+                </Button>
+              </td>
+            </tr>
           </table>
         </div>
         <div className={cx("avatar")}>
